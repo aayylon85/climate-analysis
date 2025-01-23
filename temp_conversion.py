373520a975293dbb8967ce82488bb1ffc97b1dcd
Luke Nicholas <lon1f17@soton.ac.uk>
2025-01-23 15:17:41 +0000

    Added full documentation to temperature conversion module.

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,10 +1,33 @@
-# Todo: Code is a bit unclear
+"""Module containing functions to convert from Fahrenheit temperature scale to 
+Celsius and Kelvin temprature scale.
+Will throw ValueErrors for temperatures < absolute zero.
 
-def Fc(x):
-    Y = (x - 32) * (5 / 9)
-    return Y
+Functions:
+	convert_fahr_to_celsius: Converts Farenheit to Celsius
+	convert_fahr_to_kelvin: Converts Farenheit to Kelvin
+"""
 
-def FK(x):
-    y = Fc(x)
-    z = y + 273.15
-    return z
+def convert_fahr_to_celsius(fahr):
+    """
+    Given a temperature in Fahrenheit, converts it to Celsius
+
+    :param fahr: The temperature in Fahrenheit
+    :raises ValueError: If the temperature is below absolute zero
+    :return: The temperature in Celsius
+    """
+    celsius = (fahr - 32) * (5 / 9)
+    if celsius < -273.15:
+        # If temperature is below absolute zero, throw an error
+        raise ValueError(f"Trying to convert impossible temperature: {fahr}F"
+    return celsius
+
+def convert_fahr_to_kelvin(fahr):
+    """
+    Given a temperature in Fahrenheit, converts it to kelvin
+    
+    :param fahr: The temperature in Fahrenheit
+    :return: The temperature in kelvin
+    """
+    celsius = convert_fahr_to_celsius(fahr)
+    kelvin = celsius + 273.15
+    return kelvin
